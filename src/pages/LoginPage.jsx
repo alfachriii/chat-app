f7346cae63148e7609e6cb10fe3bcdf97bfdaad6
alfachriii <xxdutt78@gmail.com>
2024-12-03 14:55:53 +0700

    login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 const LoginPage = () => {
   return (
     <>
-        <div className="bg-[url('../assets/')]"></div>
+        <div className="w-screen h-screen bg-[#FAF6E3] flex justify-center items-center">
+          <div className="w-1/3 h-96 bg-[#D8DBBD] rounded-md">
+          <form action="">
+          <h1 className="font-semibold">Login Page</h1>
+          <div className="flex flex-col">
+            <label>Email</label>
+            <input type="email" placeholder="example@example.com" />
+            <label>Password</label>
+            <input type="password" placeholder=".........." />
+
+          </div>
+          </form>
+          
+          </div>
+        </div>
     </>
   )
 }
